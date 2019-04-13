e2b8f67f0799f577bf3430523d25fd7b5d8c2cc9
SULTAN <ramhziworld@yahoo.com>
2019-04-13 20:48:26 +0100

    Added Description to PointOfInterest to test migration

---
+++ CityInfo.API/Migrations/20190413193820_SecondMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "PointOfInterest",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "dbo",
+                table: "PointOfInterest");
+        }
+    }
+}
